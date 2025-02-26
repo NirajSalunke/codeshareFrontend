db16591756b6c54b97a5363b4512a0c90f09ed7c
NirajSalunke <154681227+NirajSalunke@users.noreply.github.com>
2025-02-26 10:36:29 +0530

    codeShare Frontend init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import React from "react";
+
+const App = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center text-9xl ">
+      App
+    </div>
+  );
+};
+
+export default App;
